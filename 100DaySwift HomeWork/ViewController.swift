7d20c2fef2661d316871ba2b16f545e6fe04e05e
zorro27 <73430357+zorro27@users.noreply.github.com>
2022-04-13 22:13:30 +0300

    MVP

--- 100DaySwift HomeWork/ViewController.swift
+++ 100DaySwift HomeWork/ViewController.swift
@@ -8,12 +8,61 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var countryTableView: UITableView!
+    var nameCountryArray = [String]()
+    var nameImageArray = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        fileManager()
+        tranformArray()
     }
-
-
+    
+    func fileManager() {
+        let fm = FileManager.default
+        let patch = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: patch)
+            for item in items {
+            if item.hasSuffix("@2x.png") {
+                nameCountryArray.append(item)
+                nameImageArray.append(item)
+            }
+        }
+    }
+    
+    func tranformArray() {
+        for item in nameCountryArray {
+            let index = nameCountryArray.index(where:{$0 == item})!
+            let endIndex = item.index(item.endIndex, offsetBy: -7)
+            let transform = item.substring(to: endIndex)
+            nameCountryArray.append(transform)
+            nameCountryArray.remove(at: index)
+        }
+    }
 }
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        nameCountryArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = self.countryTableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = nameCountryArray[indexPath.row]
+        cell.imageView!.image = UIImage(named: (nameImageArray[indexPath.row]))
+        cell.imageView?.layer.borderWidth = 1
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        countryTableView.deselectRow(at: indexPath, animated: true)
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "detail") as? detailViewController {
+            vc.name = nameCountryArray[indexPath.row]
+            vc.image = nameImageArray[indexPath.row]
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
+}
+
 
